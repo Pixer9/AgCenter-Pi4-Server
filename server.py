5ad947fe0979301b4d8d673edefce0194a58d910
Kyle Boatright <kyleboatright9@gmail.com>
2023-11-30 21:44:35 -0600

    Fixed bugs

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 import utility.config as config
 from utility.logger import logger
 from typing import Dict, Any
+import datetime
 import asyncio
 import json
 
@@ -23,9 +24,9 @@ class Server(object):
                  database=None,
                  drive_writer=None,
                  local_writer=None,
-                 store_database: bool=config.STORE_DATABASE,
-                 store_local: bool=config.STORE_LOCAL,
-                 store_drive: bool=config.STORE_DRIVE) -> None:
+                 store_database: bool=config.STORE_LOCAL_DATABASE,
+                 store_local: bool=config.STORE_LOCAL_FILE,
+                 store_drive: bool=config.STORE_DRIVE ) -> None:
         self.__host = host
         self.__port = port
         self.__database = database
@@ -35,9 +36,12 @@ class Server(object):
         self._store_local = store_local
         self._store_drive = store_drive
 
-    async def _handle_data(self, data: Dict[str, Any]) -> None:
+    async def _handle_data(self, data: Dict[str, Any], timestamp: str) -> None:
         """
             Method for handling client connections asynchronously:
+                writes to database
+                writes to xlsx
+                writes to Google sheet
         """
         try:
             if data and self._store_database and self.__database:
@@ -48,7 +52,7 @@ class Server(object):
                 await self.__XLSXWriter.write_sensor_data(data)
 
             if data and self._store_drive and self.__GSWriter:
-                await self.__GSWriter.writer_sensor_data(data)
+                await self.__GSWriter.writer_sensor_data(sesnsor_dict=data, timestamp_recv=timestamp)
 
         except Exception as e:
             logger.error(f"Error handling client data: {e}")
@@ -66,23 +70,33 @@ class Server(object):
             logger.info(f"Server is listening on port {self.__port}...")
             await _server.serve_forever()
 
-    async def _process_client(self, reader, writer) -> None:
+    async def _process_client(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> None:
         """
             Method for handling client connections with asyncio.start_server
         """
         try:
-            data = await asyncio.wait_for(reader.read(config.PACKET_SIZE), timeout=Server.SERVER_TIMEOUT)
-            data_str = data.decode()
-            logger.info(f"Data received from {writer.get_extra_info('peername')}")
+            addr = writer.get_extra_info('peername')
+            logger.info(f"Now serving... {addr}")
 
-            json_data = json.loads(data_str)
-            await self._handle_data(json_data)
+            # Send current datetime so that node is synced with server
+            current_time = datetime.datetime.now().strftime("%m-%d-%Y@%H:%M:%S")
+            current_time_bytes = json.dumps(current_time).encoe("utf-8")
+            writer.write(current_time_bytes)
+            await writer.drain()
+
+            # Wait for data transmission from node
+            data = await asyncio.wait_for(reader.read(config.PACKET_SIZE), timeout=Server.SERVER_TIMEOUT)
+            json_data = json.loads(data.decode())
+            logger.info(f"Data received from {addr} at {current_time}")
+
+            await self._handle_data(data=json_data, timestamp=current_time)
+
         except asyncio.TimeoutError:
-            logger.error(f"Connection timedout for {writer.get_extra_info('peername')}")
+            logger.error(f"Connection timedout for {addr}")
         except json.JSONDecodeError as json_error:
             logger.error(f"Error decoding JSON: {json_error}")
         except Exception as e:
-            logger.error(f"Error processing client connection: {e}")
+            logger.error(f"Error processing client connection {addr}: {e}")
         finally:
             writer.close()
             await writer.wait_closed()
