5775f68b95751be0d1dc9c18b2c5c8e6c24ccb49
Kyle Boatright <kyleboatright9@gmail.com>
2023-12-05 19:29:58 -0600

    Added Google Drive image storage

--- helpers/drive_writer.py
+++ helpers/drive_writer.py
@@ -1,3 +1,5 @@
+# drive_writer.py
+from googleapiclient.http import MediaFileUpload
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
@@ -7,13 +9,8 @@ from typing import Dict, Any
 from utility.logger import logger
 import utility.config as config
 import datetime
-
-"""
-    PRIORITY:
-        When writing to Google Spreadsheet, need to ensure that data is written
-        to its correct corresponding column. Right now it is written simple as
-        the ordering it is read.
-"""
+import asyncio
+import os
 
 # TODO - In write_sensor_data, need to account for RGB list when writing to Google - [45, 135, 60]
 
@@ -22,12 +19,18 @@ class GSWriter(object):
         GSWriter handles all Google API calls for writing to a Google Spreadsheet
             spreadsheet_id --> unique ID of sheet found in URL:
                 https://docs.google.com/speradsheets/d/{SPREADSHEET_ID}/edit#grid=920914920
+
+            drive_folder_id --> unique ID of drive folder found in URL:
+                https://
     """
-    def __init__( self, spreadsheet_id: str=config.SPREADSHEET_ID ) -> None:
+    def __init__( self, spreadsheet_id: str=config.SPREADSHEET_ID, drive_folder_id: str=config.DRIVE_FOLDER_ID ) -> None:
         self.__credentials = self._authenticate()
-        self.__service = build("sheets", "v4", credentials=self.__credentials)
+        self.__sheet_service = build("sheets", "v4", credentials=self.__credentials)
+        self.__drive_service = build("drive", "v3", credentials = self.__credentials)
         self.__spreadsheet_id = spreadsheet_id
-        self.__sheet_created = []
+        self.__drive_folder_id = drive_folder_id
+        self.__lock = asyncio.Lock()
+        self.__sheets_created = []
         self.__column_mappings = {}
 
     def _authenticate(self):
@@ -49,7 +52,7 @@ class GSWriter(object):
             create it and write the appropriate headers for the data.
         """
         try:
-            sheets = self.__service.spreadsheets()
+            sheets = self.__sheet_service.spreadsheets()
             spreadsheet = sheets.get(spreadsheetId=self.__spreadsheet_id).execute()
             sheet_properties = spreadsheet.get('sheets', [])
 
@@ -76,7 +79,7 @@ class GSWriter(object):
                 header_range = f"{sheet_name}!A1:{chr(65+len(headers)-1)}"
                 header_values = [headers]
 
-                self.__service.spreadsheets().values().update(
+                self.__sheet_service.spreadsheets().values().update(
                     spreadsheetId=self.__spreadsheet_id,
                     range=header_range,
                     body={'values': header_values},
@@ -95,53 +98,49 @@ class GSWriter(object):
             Method for processing data and writing it to specified Google Sheet
                 *args -> dict sensor data, str datetime
         """
-        # DT MUST be string as it is packaged as json for transmission to Google API
-        for sensor in sensor_dict.keys():
-            if 'DT' not in sensor_dict[sensor].keys():
-                sensor_dict[sensor]['DT'] = timestamp_recv
-            elif isinstance(sensor_dict[sensor]['DT'], datetime.datetime):
-                sensor_dict[sensor]['DT'] = sensor_dict[sensor]['DT'].strftime("%m-%d-%Y@%H:%M:%S")
-            else:
-                sensor_dict[sensor]['DT'] = timestamp_recv
+        
+        # Google API is not thread safe, using Lock to help mitigate race conditions
+        async with self.__lock:
+            # DT MUST be string as it is packaged as json for transmission to Google API
+            for sensor_name, sensor_data in sensor_dict.items():
+                sheet_name = sensor_name
+                sensor_data['DT'] = timestamp_recv
 
-        for sensor_name, sensor_data in sensor_dict.items():
-            sheet_name = sensor_name
+                # Need to account for RGB values -- [45, 75, 125]
+                if sensor_name == "RGB_TCS34725":
+                    if "color_rgb_bytes" in sensor_data.keys():
+                        del sensor_data["color_rgb_bytes"]
 
-            # Need to account for RGB values
-            if sensor_name == "RGB_TCS34725":
-                if "color_rgb_bytes" in sensor_data.keys():
-                    del sensor_data["color_rgb_bytes"]
-            
-            # Sort data to match columns headers of sheet
-            ordered_dict = await self._order_values_by_sheet(sheet_name, sensor_data)
-            _values = list(ordered_dict.values())
+                if sensor_name not in self.__sheets_created:
+                    await self._create_or_clear_sheet( sensor_name, list(sensor_data.keys()) )
+                    self.__sheets_created.append(sensor_name)
 
-            if sensor_name not in self.__sheets_created:
-                await self._create_or_clear_sheet( sensor_name, list(sensor_data.keys()) )
-                self.__sheet_created.append(sensor_name)
+                # Sort data to match columns headers of sheet
+                ordered_dict = await self._order_values_by_sheet(sheet_name, sensor_data)
+                _values = list(ordered_dict.values())
 
-            range_to_append = (f"{sheet_name}!A1:{chr(65+len(list(ordered_dict.keys()))-1)}")
+                range_to_append = (f"{sheet_name}!A1:{chr(65+len(list(ordered_dict.keys()))-1)}")
 
-            try:
-                logger.info("Writing to Google Spreadsheet...")
-                self.__service.spreadsheets().values().append(
-                    spreadsheetId=self.__spreadsheet_id,
-                    range=range_to_append,
-                    body={'values': [_values]},
-                    valueInputoption='RAW'
-                ).execute()
+                try:
+                    logger.info("Writing to Google Spreadsheet...")
+                    self.__sheet_service.spreadsheets().values().append(
+                        spreadsheetId=self.__spreadsheet_id,
+                        range=range_to_append,
+                        body={'values': [_values]},
+                        valueInputoption='RAW'
+                    ).execute()
 
-                logger.info("Write successful.")
-            except HttpError as http_error:
-                logger.error(f"Error occurred while trying to write to sheet: {http_error}")
-            except RuntimeError as runtime_error:
-                logger.error(f"Runtime Error: {runtime_error} while writing to sheet {self.__spreadsheet_id}")
+                    logger.info("Write successful.")
+                except HttpError as http_error:
+                    logger.error(f"Error occurred while trying to write to sheet: {http_error}")
+                except Exception as e:
+                    logger.error(f"Error occurred while writing to Google Spreadsheet: {e}")
 
     async def _order_values_by_sheet(self, sheet_name: str, sensor_dict: Dict[str, Any]) -> dict:
         """
             Order the dictionary keys/values by the order of the sheet column names
         """
-        if sheet_name not in self.__column_mappings:
+        if sheet_name not in self.__column_mappings.keys():
             return sensor_dict
         
         key_order = self.__column_mappings[sheet_name]
@@ -157,7 +156,7 @@ class GSWriter(object):
         _range = f"{sheet_name}!1:1"
 
         try:
-            response = self.__service.spreadsheets().values().get(
+            response = self.__sheet_service.spreadsheets().values().get(
                 spreadsheetId=self.__spreadsheet_id,
                 range=_range,
                 majorDimension='ROWs'
@@ -171,3 +170,31 @@ class GSWriter(object):
             logger.exception(f"HTTP request error occurred while fetching headers: {http_error}")
         except Exception as e:
             logger.exception(f"Error occurred while fetching headers: {e}")
+
+    async def upload_to_google_drive(self, abs_image_path: str) -> None:
+        """
+            Method for pushing local images up to specified Google Drive folder
+                *args -> str absolute file path
+        """
+        async with self.__lock:
+            file_metadata = {
+                'name': os.path.basename(abs_image_path),
+                'parents': [self.__drive_folder_id]
+            }
+
+            media = MediaFileUpload(abs_image_path, resumable=True)
+
+            try:
+                logger.info("Pushing image to Google Drive folder...")
+                file = self.__drive_service.files().create(
+                    body=file_metadata,
+                    media_body=media,
+                    fields='id'
+                ).execute()
+
+                logger.info(f"Image: {abs_image_path} successfully pushed to Drive.")
+                logger.info(f"File ID: {file.get('id')}")
+            except HttpError as http_error:
+                logger.exception(f"Error occurred while trying to push media file: {http_error}")
+            except RuntimeError as runtime_error:
+                logger.exception(f"Error: {runtime_error} while working with Google Drive API.")
